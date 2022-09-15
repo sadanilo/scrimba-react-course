f018a4853d4423a32765d93a23344f30dedc0cd2
Danilo Araújo <sa.danilo@gmail.com>
2022-09-14 23:23:49 -0300

    stopped at solo project lesson

---
+++ src/components/Main.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Main() {
+  return (
+    <main>
+      <h1>Fun facts about react</h1>
+      <ul>
+        <li>2013</li>
+        <li>Jordan</li>
+        <li>100k</li>
+        <li>Facebook</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
+    </main>
+  );
+}
+
+export default Main;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "../assets/react.svg";
+
+function Navbar() {
+  return (
+    <nav>
+      <img src={logo} alt="React Logo" />
+      <h3>React Facts</h3>
+      <h4>React Course - Project 1</h4>
+    </nav>
+  );
+}
+
+export default Navbar;
